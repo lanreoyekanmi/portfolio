ec15caf4876fb2f94bd396d551e21629bb6cfc55
lanreoyekanmi <olanrewajuoyekanmi241@gmail.com>
2023-12-28 13:10:15 +0100

    JS Form submit update to send to email using formspree

--- assets/js/main.js
+++ assets/js/main.js
@@ -224,4 +224,34 @@
    */
   new PureCounter();
 
-})()
+});
+
+/**
+   * Form submit 
+   
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // Our submit logic
+});
+
+
+const data = new FormData(form);
+const values = Object.fromEntries(data.entries());
+
+// values will contain all fields
+
+const body = `Name: ${values.name} 
+              Email: ${values.email} 
+              Message: ${values.message}`;
+
+              
+
+              fetch('https://api.formspree.io/submit/', {
+  method: 'POST',
+  body: JSON.stringify({message: body})  
+})
+*/
